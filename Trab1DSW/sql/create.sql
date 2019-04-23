3def306eea012ca1661c02be4303b61af2e6e307
Berreta <felipeberreta@hotmail.com>
2019-04-22 21:16:45 -0300

    Update create.sql
    Adicionando as tabelas Admin, Site, Teatro e Promoção.

--- Trab1DSW/sql/create.sql
+++ Trab1DSW/sql/create.sql
@@ -1,11 +1,37 @@
-create table Site (
-id integer not null,
-email varchar(256) not null,
-senha varchar(256) not null,
-url varchar(256) not null,
-nome varchar(256) not null,
-telefone integer not null,
-CONSTRAINT Livro_PK PRIMARY KEY (id));
-insert into Site(id, email, senha, url, nome, telefone) values
-(1,'teste@teste', '123456', 'teste.com', 'teste1', 08008080);
+CREATE TABLE Admin (
+	cpf integer not null,
+	prenome varchar(30)  not null,
+	sobrenome varchar(30) not null,
+	email varchar(30) not null,
+	senha  varchar(30) not null,
+  constraint admin_pk primary key (cpf)
+);
 
+create table Site (
+  id integer not null,
+  email varchar(256) not null,
+  senha varchar(256) not null,
+  url varchar(256) not null,
+  nome varchar(256) not null,
+  telefone integer not null,
+  CONSTRAINT Livro_PK PRIMARY KEY (id));
+
+CREATE TABLE Teatro (
+  cnpj integer,	
+	email varchar(30) not null,
+	senha varchar(20) not null,
+	nome varchar(30) not null,
+	cidade varchar(30) not null,
+  constraint cnpj_pk primary key (cnpj)
+);
+
+CREATE TABLE Promocao(
+  id integer,
+  cnpj integer,
+	preco integer not null,
+	nomePeca varchar(100) not null,
+	dia	varchar(16) not null,
+	horario varchar(16) not null, 
+  constraint fk_id foreign key (id) references Site(id),
+  constraint fk_cnpj foreign key (cnpj) references Teatro(cnpj)
+);
